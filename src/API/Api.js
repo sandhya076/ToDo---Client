94b5d6875c9b165831fed0fe912118df565ef804
sandhya <sandhya.lavuri07@gmail.com>
2022-02-15 09:01:38 +0530

    first change

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://todo-serve.herokuapp.com";
+const url = "https://todo-serve.herokuapp.com/list";
 export const fetchList = () => axios.get(url)
 export const createList = (newlist)=> axios.post(url,newlist);
 export const updateList =(id,updatelist)=>axios.patch(`${url}/${id}`,updatelist);
